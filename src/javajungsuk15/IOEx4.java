e8e35af02fd619babe331624b4ead1acf2eb3c8b
jayeon Son <sjyun0000@naver.com>
2022-05-11 19:26:21 +0900

    javajungsuk

---
+++ src/javajungsuk15/IOEx4.java
@@ -0,0 +1,30 @@
+package javajungsuk15;
+
+import java.io.*;
+import java.util.Arrays;
+
+class IOEx4 {
+    public static void main(String[] args) {
+        byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+        byte[] outSrc = null;
+        byte[] temp = new byte[4];
+
+        ByteArrayInputStream input = null;
+        ByteArrayOutputStream output = null;
+
+        input = new ByteArrayInputStream(inSrc);
+        output = new ByteArrayOutputStream();
+
+        try {
+            while(input.available() > 0) {
+                int len = input.read(temp);
+                output.write(temp, 0, len);
+            }
+        } catch(IOException e) {}
+        outSrc = output.toByteArray();
+
+        System.out.println("input Source:" + Arrays.toString(inSrc));
+        System.out.println("temp : " + Arrays.toString(temp));
+        System.out.println("Output : " + Arrays.toString(outSrc));
+    }
+}
